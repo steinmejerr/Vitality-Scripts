72eb29b975041d6f95e13acff715753df33eb621
Jonas Steinmejer <jonas04@outlook.dk>
2026-07-18 14:28:44 +0200

    Fixed colors

--- [OVEREXTENDED]/ox_inventory/init.lua
+++ [OVEREXTENDED]/ox_inventory/init.lua
@@ -15,7 +15,7 @@ end
 shared = {
     resource = GetCurrentResourceName(),
     framework = GetConvar('inventory:framework', 'esx'),
-    playerslots = GetConvarInt('inventory:slots', 50),
+    playerslots = GetConvarInt('inventory:slots', 60),
     playerweight = GetConvarInt('inventory:weight', 30000),
     target = GetConvarBool('inventory:target', false),
     police = json.decode(GetConvar('inventory:police', '["police", "sheriff"]')),
