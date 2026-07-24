cf4c1a73beece12a1a3d386bc28cda9d6f77d0d5
Jonas Steinmejer <jonas04@outlook.dk>
2026-07-24 11:39:58 +0200

    Added new prison mlo 5

---
+++ [MAPS]/prompt_prison_tower_23/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+game 'gta5'
+this_is_a_map 'yes'
+
+author 'Prompt Studio'
+description 'Additions to Prompt Prison'
+version '1.0.0'
+
+escrow_ignore {
+    'stream/garage/unlocked/**',
+    'stream/garden/unlocked/**',
+    'stream/prison_tower_green/unlocked/**'
+}
+dependency '/assetpacks'
