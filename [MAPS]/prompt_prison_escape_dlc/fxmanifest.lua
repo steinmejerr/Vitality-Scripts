143d6dd0363d8950b1675d38def01626b0f71c5e
Jonas Steinmejer <jonas04@outlook.dk>
2026-07-24 11:39:24 +0200

    Added new prison mlo 3

---
+++ [MAPS]/prompt_prison_escape_dlc/fxmanifest.lua
@@ -0,0 +1,24 @@
+lua54 'yes'
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Prompt Studio'
+description 'Escape System for Prompt Prison'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+
+escrow_ignore {
+  'stream/rd_pris_back/unlocked/**',
+  'stream/rd_pris_ext//**',
+  'stream/rd_pris_mine/unlocked/**',
+  'stream/rd_pris_pipes/unlocked/**',
+  'stream/rd_pris_sawagenode/unlocked/**'
+}
+
+
+--#client_scripts {
+--#	'client.lua'
+--#}
+dependency '/assetpacks'
