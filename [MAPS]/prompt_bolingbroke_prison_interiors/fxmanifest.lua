f7a41d622385c2672b86bf40ed25f0d19b43512a
Jonas Steinmejer <jonas04@outlook.dk>
2026-07-24 11:38:37 +0200

    Added a new prison mlo 1

---
+++ [MAPS]/prompt_bolingbroke_prison_interiors/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'bodacious'
+game 'gta5'
+this_is_a_map 'yes'
+
+
+data_file 'DLC_ITYP_REQUEST' 'stream/prompt_prison_escapeprops.ytyp'
+
+escrow_ignore {
+  'stream/*.ytd',
+  'stream/*.ymap',
+  'stream/*.ytyp',
+  'stream/unlocked/**',
+  'stream/vanilla/**',
+  'stream/newplacement/**'
+}
+
+dependency '/assetpacks'
