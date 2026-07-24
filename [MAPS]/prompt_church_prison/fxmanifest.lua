db421a0faeddf1c7b513ffac0bb4cb68eec8599d
Jonas Steinmejer <jonas04@outlook.dk>
2026-07-24 11:39:04 +0200

    Added new prison mlo 2

---
+++ [MAPS]/prompt_church_prison/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'Prompt Studio | Igro45'
+description 'Chruch in prison Sandy Shores 2025'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+  'stream/unlocked/**'
+}
+
+-- data_file 'DLC_ITYP_REQUEST' 'int_corporate.ytyp'
+dependency '/assetpacks'
